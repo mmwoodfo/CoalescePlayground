2c28e501aef437fca9c69d8293ecf40932ec19cc
Meghan <mmwoodfo@asu.edu>
2020-06-19 15:31:47 -0700

    Cleaned up UI

--- CoalescePlayground.Web/src/views/ViewUsers.vue
+++ CoalescePlayground.Web/src/views/ViewUsers.vue
@@ -1,31 +1,46 @@
 <template>
+
   <div>
+
     <button v-if="createMode==false" class="crudBtns" @click="enterCreateMode">Add Person</button>
     <button v-if="createMode==true" class="crudBtns" @click="enterCreateMode">Cancel Person</button>
+
     <div v-if="createMode==true">
-      Name: <input v-model="person.name" />
-      <!--DOB: <c-datetime-picker v-model="person.birthDate" date-kind="date" />
-    Middle Name: <input v-model="person.middleName" />-->
-      <button class="crudBtns" @click="createPerson()">+</button>
+      Name: <input class="crudInputs" v-model="person.name" />
+      DOB: <c-datetime-picker v-model="person.birthDate" date-kind="date" />
+      Middle Name: <input class="crudInputs" v-model="person.middleName" />
+      <button class="crudBtns" @click="createPerson()">Submit</button>
     </div>
+
     <ol>
       <span v-for="people in personList.$items">
+
+        <!--Person Name-->
         <li>
           <span v-if="person.personId == null || person.personId != people.personId">{{people.name}}</span>
-          <span v-if="person.personId != null && person.personId == people.personId"><input v-on:keyup.enter="updatePerson()" v-model="person.name" /></span>
+          <span v-if="person.personId != null && person.personId == people.personId"><input class="crudInputs" v-on:keyup.enter="updatePerson()" v-model="person.name" /></span>
           <button @click="editPerson(people)" class="crudBtns">&#x270E;</button> <!--edit-->
           <button @click="deletePerson(people)" class="crudBtns">&#x1F5D1;</button> <!--delete-->
+          <button v-if="person.personId !=null && person.personId == people.personId" @click="newPerson()" class="crudBtns">X</button> <!--cancel-->
         </li>
-        <ul>
-          <li>
-            <span v-if="person.personId == null || person.personId != people.personId">DOB: <c-display :model="people" for="birthDate" format="M/d/yyyy" /></span>
-            <span v-if="person.personId != null && person.personId == people.personId"><c-datetime-picker v-on:keyup.enter="updatePerson()" v-model="person.birthDate" date-kind="date" /></span>
-          </li>
-          <li>Middle Initial: {{people.middleName}}</li>
-        </ul>
+          <ul>
+            <li>
+              <!--Person DOB-->
+              <span v-if="person.personId == null || person.personId != people.personId">DOB: <c-display :model="people" for="birthDate" format="M/d/yyyy" /></span>
+              <span v-if="person.personId != null && person.personId == people.personId"><c-datetime-picker v-on:keyup.enter="updatePerson()" v-model="person.birthDate" date-kind="date" /></span>
+            </li>
+            <!--Person Middle Initial-->
+            <li>
+              <span v-if="person.personId == null || person.personId != people.personId">Middle Name: {{people.middleName}}</span>
+              <span v-if="person.personId != null && person.personId == people.personId"><input class="crudInputs" v-on:keyup.enter="updatePerson()" v-model="person.middleName" /></span>
+            </li>
+          </ul>
+
       </span>
     </ol>
+
   </div>
+
 </template>
 
 <script lang="ts">
@@ -45,6 +60,10 @@
       this.personList.$load();
     }
 
+    newPerson() {
+      this.person = new PersonViewModel();
+    }
+
     enterCreateMode() {
       this.createMode = !this.createMode;
     }
@@ -56,7 +75,7 @@
       this.person.$save().then(() => {
         this.createMode = false;
         this.personList.$load();
-        this.person = new PersonViewModel();
+        this.newPerson();
       });
     }
 
@@ -66,8 +85,7 @@
       //alert('saved');
 
       //$save : coalesce save
-      this.person.$save().then(() => { alert('Updated') });
-      this.person = new PersonViewModel();
+      this.person.$save().then(() => { this.newPerson(); });
     }
 
     deletePerson(person: PersonViewModel) {
@@ -89,13 +107,19 @@
     box-shadow: 3px 5px 2px rgba(0, 0, 0, 0.5);
   }
 
-    .crudBtns:hover {
-      color: white;
-      background-color: black;
-      box-shadow: none;
-    }
+  .crudBtns:hover {
+    color: white;
+    background-color: black;
+    box-shadow: none;
+  }
 
-  input {
-    background-color: lightblue;
+  .crudInputs {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+    border-radius: 10px;
   }
 </style>
